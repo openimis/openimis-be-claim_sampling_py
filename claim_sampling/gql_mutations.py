4ef14d7f291eabd56e9d926f1575127676d3484e
wzglinieckisoldevelo <wzgliniecki@soldevelo.com>
2023-11-07 12:38:10 +0100

    ONI-159: Basic version of claim sampling feature.

---
+++ claim_sampling/gql_mutations.py
@@ -0,0 +1,193 @@
+import logging
+import uuid
+import pathlib
+import base64
+from typing import Callable, Dict
+import random
+import graphene
+
+from .apps import ClaimSamplingConfig
+from core.schema import TinyInt, OpenIMISMutation
+from django.contrib.auth.models import AnonymousUser
+from django.core.exceptions import ValidationError, PermissionDenied
+from django.utils.translation import gettext as _
+
+from claim.gql_mutations import ClaimCodeInputType, ClaimGuaranteeIdInputType, FeedbackInputType
+from claim.models import ClaimAdmin
+
+from django.db import transaction
+
+from .models import ClaimSamplingBatch, ClaimSamplingBatchAssignment
+
+logger = logging.getLogger(__name__)
+
+
+class ClaimSamplingBatchInputType(OpenIMISMutation.Input):
+    percentage = graphene.Int(required=True)
+    claimAdminUuid = graphene.String(required=True)
+
+    status = TinyInt(required=False)
+    id = graphene.Int(required=False, read_only=True)
+    uuid = graphene.String(required=False)
+    autogenerate = graphene.Boolean(required=False)
+    insuree_id = graphene.Int(required=False)
+    date_from = graphene.Date(required=False)
+    date_to = graphene.Date(required=False)
+    icd_id = graphene.Int(required=False)
+    icd_1_id = graphene.Int(required=False)
+    icd_2_id = graphene.Int(required=False)
+    icd_3_id = graphene.Int(required=False)
+    icd_4_id = graphene.Int(required=False)
+    review_status = TinyInt(required=False)
+    date_claimed = graphene.Date(required=False)
+    date_processed = graphene.Date(required=False)
+    health_facility_id = graphene.Int(required=False)
+    refer_from_id = graphene.Int(required=False)
+    refer_to_id = graphene.Int(required=False)
+    batch_run_id = graphene.Int(required=False)
+    category = graphene.String(max_length=1, required=False)
+    visit_type = graphene.String(max_length=1, required=False)
+    admin_id = graphene.Int(required=False)
+    explanation = graphene.String(required=False)
+    adjustment = graphene.String(required=False)
+    json_ext = graphene.types.json.JSONString(required=False)
+    restore = graphene.UUID(required=False)
+    feedback_available = graphene.Boolean(default=False)
+    feedback_status = TinyInt(required=False)
+    care_type = graphene.String(required=False)
+
+    # code = graphene.Field(ClaimCodeInputType, required=True)
+    # feedback = graphene.Field(FeedbackInputType, required=False)
+    # guarantee_id = ClaimGuaranteeIdInputType(required=False)
+
+
+
+
+
+@transaction.atomic
+def update_or_create_claim_sampling_batch(data, user):
+    claim_sampling_batch_uuid = data.pop("uuid", None)
+
+    claim_sampling_batch_data = {'assigned_to': ClaimAdmin.objects.get(uuid=data.get("claimAdminUuid")),
+                                 'created_by': ClaimAdmin.objects.get(uuid=data.pop("claimAdminUuid")),#user.id
+                                 'is_completed': False,
+                                 'is_applied': False,
+                                 'computed_value':  {},
+                                 'assigned_value':  {},
+                                 }
+
+    if claim_sampling_batch_uuid is not None:
+        claim_sampling_batch = ClaimSamplingBatch.objects.get(uuid=claim_sampling_batch_uuid)
+        claim_sampling_batch.save_history()
+        # reset the non required fields
+        # (each update is 'complete', necessary to be able to set 'null')
+        [setattr(claim_sampling_batch, key, claim_sampling_batch_data[key]) for key in claim_sampling_batch_data]
+    else:
+        claim_sampling_batch = ClaimSamplingBatch.objects.create(**claim_sampling_batch_data)
+    claim_sampling_batch.save()
+
+    create_claim_sampling_batch_assignment(data=data, claim_sampling_batch=claim_sampling_batch)
+
+    return claim_sampling_batch
+
+
+def create_claim_sampling_batch_assignment(data, claim_sampling_batch):
+    from claim_sampling.services import get_claims_from_data_helper
+    percentage = data.pop('percentage')
+    claim_batch_ids = get_claims_from_data_helper(data)#[claim.id for claim in get_claims_from_data_helper(data)]
+    data['claim_batch_id'] = claim_sampling_batch.id
+    data['claim_id'] = claim_batch_ids
+    status_options = [ClaimSamplingBatchAssignment.Status.IDLE, ClaimSamplingBatchAssignment.Status.SKIPPED]
+    status_list = random.choices(status_options, cum_weights=[percentage, 100], k=len(claim_batch_ids))
+
+    # claim_sampling_batch_assignments = [ClaimSamplingBatchAssignment(claim_batch_id=claim_sampling_batch.id,
+    #                                                                  claim_id=status_list[])]
+    claim_sampling_batch_assignments = []
+
+    for idx, claim_id in enumerate(claim_batch_ids):
+        claim_sampling_batch_assignments.append(ClaimSamplingBatchAssignment(claim_batch_id=claim_sampling_batch,
+                                                                             claim_id=claim_id,
+                                                                             status=status_list[idx]))
+
+    assignments = ClaimSamplingBatchAssignment.objects.bulk_create(claim_sampling_batch_assignments)
+
+    # claim_sampling_batch_assignment.save()
+    return assignments #claim_sampling_batch_assignment
+
+
+class CreateClaimSamplingBatchMutation(OpenIMISMutation):
+    """
+    Create a new claim sampling batch.
+    """
+    _mutation_module = "claim_sampling"
+    _mutation_class = "CreateClaimSamplingBatchMutation"
+
+    class Input(ClaimSamplingBatchInputType):
+        pass
+
+    @classmethod
+    def async_mutate(cls, user, **data):
+        try:
+            if type(user) is AnonymousUser or not user.id:
+                raise ValidationError(_("mutation.authentication_required"))
+            # if not user.has_perms(ClaimSamplingConfig.gql_mutation_create_claim_batch_samplings_perms):
+            #     raise PermissionDenied(_("unauthorized"))
+            if "client_mutation_id" in data:
+                data.pop('client_mutation_id')
+            if "client_mutation_label" in data:
+                data.pop('client_mutation_label')
+            # data['audit_user_id'] = user.id_for_audit
+            from core.utils import TimeUtils
+            # data['validity_from'] = TimeUtils.now()
+            claim_sampling_batch = update_or_create_claim_sampling_batch(data, user)
+            return None
+        except Exception as exc:
+            return [{
+                'message': _("claim.mutation.failed_to_create_claim_sampling_batch") % {'code': data['code']},
+                'detail': str(exc)}]
+
+
+class UpdateClaimSamplingBatchMutation(OpenIMISMutation):
+    """
+    Update a claim. The claim items and services can all be updated with this call
+    """
+    _mutation_module = "claim_sampling"
+    _mutation_class = "UpdateClaimSamplingBatchMutation"
+
+    class Input(ClaimSamplingBatchInputType):
+        pass
+
+    @classmethod
+    def async_mutate(cls, user, **data):
+        try:
+            if type(user) is AnonymousUser or not user.id:
+                raise ValidationError(
+                    _("mutation.authentication_required"))
+            if not user.has_perms(ClaimSamplingConfig.gql_mutation_update_claim_batch_samplings_perms):
+                raise PermissionDenied(_("unauthorized"))
+            data['audit_user_id'] = user.id_for_audit
+            update_or_create_claim_sampling_batch(data, user)
+            return None
+        except Exception as exc:
+            return [{
+                'message': _("claim.mutation.failed_to_update_claim_sampling_batch") % {'code': data['code']},
+                'detail': str(exc)}]
+
+
+class ApproveClaimSamplingBatchMutation(OpenIMISMutation):
+    """
+    Approve given claim batch and apply deduction rate or other parameters across all claims in given batch.
+    """
+    _mutation_module = "claim_sampling"
+    _mutation_class = "ApproveClaimSamplingBatchMutation"
+
+    class Input(ClaimSamplingBatchInputType):
+        pass
+
+    @classmethod
+    def async_mutate(cls, user, **data):
+        if not user.has_perms(ClaimSamplingConfig.gql_mutation_approve_claim_batch_samplings_perms):
+            raise PermissionDenied(_("unauthorized"))
+        errors = []
+
+        return errors
--- claim_sampling/services.py
+++ claim_sampling/services.py
@@ -1,16 +1,95 @@
-import logging
-import warnings
-
-logger = logging.getLogger(__name__)
-
-# Remove this code when implementing services
-warnings.warn("The example code in service is still present.")
+from claim.apps import ClaimConfig
+from claim.models import Claim
+from enum import Enum
+from django.db.models import OuterRef, Subquery, Avg, Q
 
 
-def example_service_function_job():
-    pass
+def get_claims_from_data_helper(kwargs):
+    '''
+    This function is copy of resolver from claim method, ideally it should be rewritten, so both endpoints use the same
+    code.
+    :return:
+    '''
 
+    query = Claim.objects.all()
+    code_is_not = kwargs.get("code_is_not", None)
+    if code_is_not:
+        query = query.exclude(code=code_is_not)
+    variance = kwargs.get("diagnosisVariance", None)
 
-class ExampleService:
-    def example_service_method_job(self):
-        pass
+    show_restored = kwargs.get("show_restored", None)
+    if show_restored:
+        query = query.filter(restore__isnull=False)
+
+    items = kwargs.get("items", None)
+    services = kwargs.get("services", None)
+
+    if items:
+        query = query.filter(items__item__code__in=items)
+
+    if services:
+        query = query.filter(services__service__code__in=services)
+
+    attachment_status = kwargs.get("attachment_status", 0)
+
+    class AttachmentStatusEnum(Enum):
+        NONE = 0
+        WITH = 1
+        WITHOUT = 2
+
+    if attachment_status == AttachmentStatusEnum.WITH.value:
+        query = query.filter(attachments__isnull=False)
+    elif attachment_status == AttachmentStatusEnum.WITHOUT.value:
+        query = query.filter(attachments__isnull=True)
+
+    care_type = kwargs.get("care_type", None)
+
+    if care_type:
+        query = query.filter(care_type=care_type)
+
+    json_ext = kwargs.get("json_ext", None)
+
+    if json_ext:
+        query = query.filter(json_ext__jsoncontains=json_ext)
+
+    if variance:
+        from core import datetime, datetimedelta
+
+        last_year = datetime.date.today() + datetimedelta(years=-1)
+        diag_avg = (
+            Claim.objects.filter(validity_to__isnull=True)
+            .filter(date_claimed__gt=last_year)
+            .values("icd__code")
+            .filter(icd__code=OuterRef("icd__code"))
+            .annotate(diag_avg=Avg("approved"))
+            .values("diag_avg")
+        )
+        variance_filter = Q(claimed__gt=(
+            1 + variance / 100) * Subquery(diag_avg))
+        if not ClaimConfig.gql_query_claim_diagnosis_variance_only_on_existing:
+            diags = (
+                Claim.objects.filter(validity_to__isnull=True)
+                .filter(date_claimed__gt=last_year)
+                .values("icd__code")
+                .distinct()
+            )
+            variance_filter = variance_filter | ~Q(icd__code__in=diags)
+        query = query.filter(variance_filter)
+
+    # TODO check if replacement is valid in every instance
+    # return gql_optimizer.query(query.all(), info)
+
+    # qs = super(DjangoFilterConnectionField, cls).resolve_queryset(
+    #     connection, iterable, info, args
+    # )
+    # filter_kwargs = {k: v for k, v in args.items() if k in filtering_args}
+    # qs = filterset_class(data=filter_kwargs, queryset=qs, request=info.context).qs
+    #
+    # return OrderedDjangoFilterConnectionField.orderBy(qs, args)
+    # if 'status' in kwargs:
+    #     query = query.filter(status=kwargs['status'])
+
+    if kwargs is not None:
+        query = query.filter(**kwargs)
+
+    return query.filter(validity_to__isnull=True)
